784815ca492ca954dcc99d83339ea0d72c46ace1
EllaGospel <ella4real5@gmail.com>
2023-01-16 20:35:03 +0100

    addition

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,20 +2,53 @@
 function Skills() {
   return (
     <>
-    <h2 >My Skills</h2>
+    <div class="w-full xl:ml-8 2xl:ml-8 lg:ml-8">
+                        <h3 class="mb-6 text-lg font-bold mt-0 xl:mt-0 2xl:mt-0 lg:mt-0 ">My Skills</h3>
+                        <div class="flex flex-col space-y-4">
+                            <div class="flex space-x-4">
+                                <div class="flex items-center justify-center w-full px-3 py-3 bg-zinc-200 text-gray-800 font-semibold rounded">
+                                    <span>HTML</span>
+                                </div>
+                                <div class="flex items-center justify-center w-full px-3 py-3 bg-zinc-200 text-gray-800 font-semibold rounded">
+                                    <span>CSS</span>
+                                </div>
+                                <div class="flex items-center justify-center w-full px-3 py-3 bg-zinc-200 text-gray-800 font-semibold rounded"><span>php</span>
+                                </div>
+                                <div class="flex items-center justify-center w-full px-3 py-3 bg-zinc-200 text-gray-800 font-semibold rounded"><span>laravel</span>
+                                </div>
+                            </div>
+                            <div class="flex space-x-4">
+                                <div class="flex items-center justify-center w-full px-3 py-3 bg-zinc-200 text-gray-800 font-semibold rounded">
+                                    <span>JavaScript</span>
+                                </div><div class="flex items-center justify-center w-full px-3 py-3 bg-zinc-200 text-gray-800 font-semibold rounded">
+                                    <span>React</span>
+                                </div>
+                                <div class="flex items-center justify-center w-full px-3 py-3 bg-zinc-200 text-gray-800 font-semibold rounded"><span>Git</span>
+                                </div>
+                                <div class="flex items-center justify-center w-full px-3 py-3 bg-zinc-200 text-gray-800 font-semibold rounded">
+                                    <span>GitHub</span></div></div><div class="flex space-x-4"><div class="flex items-center justify-center w-full px-3 py-3 bg-zinc-200 text-gray-800 font-semibold rounded">
+                                        <span>Tailwind CSS</span>
+                                    </div>
+                                <div class="flex items-center justify-center w-full px-3 py-3 bg-zinc-200 text-gray-800 font-semibold rounded">
+                                    <span>Responsive Design</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+    {/* <h2 >My Skills</h2>
     <div className="skill">
         <p className="skill-list box">HTML</p>
         <p className="skill-list box">CSS</p>
         <p className="skill-list box">JavaScript</p>
         </div>
-        <div className="skill1">
+        <div className="">
         <p className="skill-list1 box1">React</p>
         <p className="skill-list1 box1">Git</p>
         <p className="skill-list1 box1">GitHub</p>
         </div>
-        <div  className="skill-2">
+        <div>
         <p className="skill-list1 box2">Responsive Design</p>
-    </div>
+    </div> */}
     </>
   )
 }
