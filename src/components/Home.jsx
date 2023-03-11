161a622ab35e01575586dfcd82ae657c70aca937
ViktoreNnamdi <nnamdyvictor@gmail.com>
2023-03-11 17:45:37 +0100

    repositioning of projects

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,7 +11,7 @@ function Home() {
       </div>
       <div className="intro">
       <h1 className="head">HI, I'M GOSPEL EMMANUELA CHARLES</h1>
-      <p className="personality">A frontend Developer focused on building  frontend user interface that leads to  success overall product</p>
+      <p className="personality">A frontend Developer focused on building  frontend user interface that leads to the success of overall product</p>
       <Link to='/Project' className="project-link">projects</Link>
       </div>
       </div>
